928402537417a96badf5670ad3fa052e8551b8b1
Geoffrey Hannigan <ghanni@upenn.edu>
2016-09-11 18:29:09 -0400

    Cleaned up script with params.

--- ccontigs.jl
+++ ccontigs.jl
@@ -14,6 +14,12 @@ argx = ArgParseSettings()
         help = "Input fasta file for identifying circular contigs."
     "--output", "-o"
         help = "Output file to write reults."
+    "--length", "-l"
+        help = "Length of sequence window for detecting repeats."
+        default = 100
+    "--thresh", "-t"
+        help = "Threshold (percent of length) for circular contig."
+        default = 0.97
 end
 
 parsed_args = parse_args(ARGS, argx)
@@ -30,10 +36,18 @@ scoremodel = AffineGapScoreModel(
 filepath = parsed_args["input"]
 fileout = parsed_args["output"]
 outputfile = open(fileout, "w")
+hitlength = parsed_args["length"]
+threshold = hitlength * 5 * parsed_args["thresh"]
+maxscore = hitlength * 5
+print("Perfect alignment score is $maxscore\n")
 
 for s in open( filepath, FASTA )
-    alignment_result = pairalign(problem, s.seq[1:50], s.seq[51:end], scoremodel)
+    alignment_result = pairalign(problem, s.seq[1:hitlength], s.seq[hitlength:end], scoremodel)
     scorestring = score(alignment_result)
     seqname = s.name
-    write(outputfile, "$scorestring\t$seqname\n")
+    if scorestring > threshold
+        write(outputfile, "$seqname\t$scorestring\n")
+    end
 end
+
+print("Complete\n")
